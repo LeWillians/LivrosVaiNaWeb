66c390ed5c6d9b83d40bd8874d73f6d98cd43e8d
LeWillians <leandrareis53@gmail.com>
2024-11-25 19:28:45 -0300

    Adicionando segunda sessão do Início e Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from './Components/header/Header'
 import './globalStyle/globalStyle.scss'
+import Footer from './Components/footer/Footer'
 
 export default function App(){
   return(
+    <>
     <Header/>
+    <Footer/>
+    </>
   )
 }
